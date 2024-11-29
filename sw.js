8945f81d9b5a9fbe313e4bb7b3e94048f4addd07
3197516865@qq.com <root@192.168.10.128>
2024-11-29 20:46:11 +0800

    Site updated: 2024-11-29 20:46:11

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@ const preCache = [
   "/css/style.css",
   "/js/script.js",
 ];
-const VERSION = "1732881957162";
+const VERSION = "1732884369564";
 const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
